4d3517b030a8d0dfcb8d3b2e407097c61194ce1b
Santiago Montano <slincem@gmail.com>
2024-01-21 23:34:39 -0500

    adding routing for inventory service

--- src/main/java/guru/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/guru/sfg/gateway/config/LocalHostRouteConfig.java
@@ -15,6 +15,8 @@ public class LocalHostRouteConfig {
                         .uri("http://localhost:8080"))
                 .route(r -> r.path("/api/v1/customers/**")
                         .uri("http://localhost:8081"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("http://localhost:8082"))
                 .build();
     }
 }

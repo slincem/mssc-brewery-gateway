f37629da34dc3da871cd3250e727c4bd5bbe41a8
Santiago Montano <slincem@gmail.com>
2024-01-29 22:26:42 -0500

    added circuit breaker config

--- src/main/java/guru/sfg/gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/guru/sfg/gateway/config/LoadBalancedRoutesConfig.java
@@ -1,10 +1,14 @@
 package guru.sfg.gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.route.builder.UriSpec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.function.Function;
 
 @Profile("local-discovery")
 @Configuration
@@ -18,7 +22,19 @@ public class LoadBalancedRoutesConfig {
                 .route(r -> r.path("/api/v1/customers/**")
                         .uri("lb://order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .filters(this.filterInventoryFailoverCircuitBreaker())
                         .uri("lb://inventory-service"))
+                .route(r -> r.path("/inventory-failover/**")
+                        .uri("lb://inventory-failover"))
                 .build();
+    }
+
+    Function<GatewayFilterSpec, UriSpec> filterInventoryFailoverCircuitBreaker() {
+
+        return f -> f.circuitBreaker(config ->
+                config
+                        .setName("inventoryCB")
+                        .setFallbackUri("forward:/inventory-failover")
+                        .setRouteId("inv-failover"));
     }
 }
